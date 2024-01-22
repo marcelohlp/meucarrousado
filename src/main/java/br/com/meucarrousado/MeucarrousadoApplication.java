5ee1aed7ea52758ebafdbd2050c67bb26885ab32
Marcelo Paschoal <marcelo.hlp@outlook.com>
2024-01-22 09:56:29 -0300

    Preparando ambiente

---
+++ src/main/java/br/com/meucarrousado/MeucarrousadoApplication.java
@@ -0,0 +1,13 @@
+package br.com.meucarrousado;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeucarrousadoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeucarrousadoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/meucarrousado/MeucarrousadoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.meucarrousado;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MeucarrousadoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
